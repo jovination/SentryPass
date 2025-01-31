0b87303d83a40e6a5cc2f6eba5ac44477132bc75
jovination <jovination791@gmail.com>
2025-01-31 15:11:51 +0300

    few changes

--- components/FeaturedCard.tsx
+++ components/FeaturedCard.tsx
@@ -9,7 +9,7 @@ interface FeaturedCardProps {
 
 export function FeaturedCard({ imageSrc, imageAlt, title, description }: FeaturedCardProps) {
   return (
-    <div className="w-full lg:w-[320px] h-auto min-h-[296px] bg-white rounded-[36px] flex flex-col items-center space-y-4 px-6 py-8 border border-[#FFDDBF]">
+    <div className="w-full   md:w-[448px] lg:w-[320px] h-auto min-h-[296px] bg-white rounded-[36px] flex flex-col items-center space-y-4 px-6 py-8 border border-[#FFDDBF]">
       <Image src={imageSrc || "/placeholder.svg"} alt={imageAlt} width={80} height={80} />
       <p className="font-semibold">{title}</p>
       <p className="text text-center">{description}</p>
